830c9a29eede0346231c251a80c28267c6f0f4d9
amaddix <60373214+amaddix@users.noreply.github.com>
2021-03-13 21:17:52 -0800

    added an about page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Joi Ride' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Joi Ride" });
+});
+
+// about page
+router.get("/about", function (req, res, next) {
+  res.render("about", { title: "about page" });
 });
 
 module.exports = router;
