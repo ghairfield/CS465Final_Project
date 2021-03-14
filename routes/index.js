379a47f0b61c9c216fcf8ecdef3f32b6fefd5c27
Greg Hairfield <ghairfield@gmail.com>
2021-03-13 19:50:25 -0800

    Removed google maps key

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Joi Ride' });
+  let key = process.env.GOOGLE_API;
+  res.render('index', { 
+    title: 'Joi Ride', 
+    google_key: key });
 });
 
 module.exports = router;
